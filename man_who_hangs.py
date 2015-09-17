ee4b885d9ede8e16aaa138c5bc3adea4054ccc78
Zach Seaborn <zseaborn63@gmail.com>
2015-09-17 17:54:00 -0400

    Normal Mode working and polished

--- man_who_hangs.py
+++ man_who_hangs.py
@@ -25,15 +25,31 @@ empty_spaces_string = empty_spaces * "_ "
 
 empty_spaces_list = empty_spaces_string.split()
 
+previous_guesses = []
+
+round_counter = 7
+print("You think you can beat me, stupid human?")
+print("The word is {} letters long\n".format(len(guess_word)))
 while empty_spaces != 0:
-    previous_guesses = []
-    print(empty_spaces_string))
-    user_character_guess = input("What letter is your guess?  ").lower()
-    previous_guesses.append(user_character_guess)
+    print(" ".join(empty_spaces_list))
+    user_character_guess = input("What letter is your guess?  \n").lower()
     if user_character_guess in previous_guesses:
-        print("You already guessed that one!")
-    if user_character_guess in guess_word:
+        print("You already guessed that one!\n")
+    elif user_character_guess in guess_word:
+        previous_guesses.append(user_character_guess)
         occurance_index = find_occurances(guess_word, user_character_guess)
+        empty_spaces = empty_spaces - len(occurance_index)
         for occurance in occurance_index:
-            empty_spaces_list[x] = user_character_guess
+            empty_spaces_list[occurance] = user_character_guess
             new_empty_spaces_string = " ".join(empty_spaces_list)
+    elif user_character_guess == 'give up':
+        print("The word I was using was {}".format(guess_word))
+        print("YOU LOSE HUMAN!")
+        break
+    else:
+        previous_guesses.append(user_character_guess)
+        print("That letter isn't in my word! \n")
+
+if empty_spaces == 0:
+    print(" ".join(empty_spaces_list))
+    print("YOU BEAT ME HUMAN!!")
