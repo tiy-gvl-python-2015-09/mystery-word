f6682dc394a0d2c1d97c89814eca1d4cf54ad9ce
andbar <andbar@users.noreply.github.com>
2015-09-17 15:07:06 -0400

    Added function to create guessed word to display to user

--- hanging_man.py
+++ hanging_man.py
@@ -7,7 +7,15 @@ word_length = len(game_word)
 
 word_as_list = list(game_word)
 
-guessed_word_as_list = ["_", "_", "_", "_", "_"]
+#guessed_word_as_a_list = ["_", "_", "_", "_", "_"]
+
+def create_guessed_word(random_word): #creates guessed word same length as random word
+    guessed_word_as_list = []
+    count = 0
+    while count < len(random_word):
+        guessed_word_as_list.append("_")
+        count += 1
+    return guessed_word_as_list
 
 def check_letter(passed_letter): #checks a letter with game word and returns game word with correct guesses revealed
     letter = passed_letter.upper()
@@ -15,4 +23,6 @@ def check_letter(passed_letter): #checks a letter with game word and returns gam
         for i, l in enumerate(word_as_list):
             if l == letter:
                 guessed_word_as_list[i] = l
+        return guessed_word_as_list
+    else:
         return guessed_word_as_list
