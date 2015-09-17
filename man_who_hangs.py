9787a7ae6bdb6adafa0d46c60985b7ef4677f962
Zach Seaborn <zseaborn63@gmail.com>
2015-09-17 13:01:55 -0400

    Computer guesses word is functional

--- man_who_hangs.py
+++ man_who_hangs.py
@@ -1,1 +1,14 @@
 # This will be my version of Hang-Man
+from random import randint
+
+with open("/usr/share/dict/words") as infile:
+    available_words = infile.read()
+
+word_list = available_words.split('\n')
+
+how_many_words = len(word_list)
+
+guess_number = randint(0, how_many_words)
+guess_word = word_list[guess_number]
+
+empty_spaces = len(guess_word)
