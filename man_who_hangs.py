52f581387e03f7d20d372d905240ecaed85736cf
zseaborn63 <zseaobnr63@gmail.com>
2015-09-18 11:09:56 -0400

    Improved number of rounds code

--- man_who_hangs.py
+++ man_who_hangs.py
@@ -25,14 +25,22 @@ def get_spaces_list(word):
     return spaces_list
 
 
-def man_who_hangs_game(spaces, guess_word):
+def man_who_hangs_game(infile):
+    guess_word = make_computer_guess_word(infile)
+    empty_spaces_list = get_spaces_list(guess_word)
     print("You think you can beat me, stupid human?")
     print("The word is {} letters long\n".format(len(guess_word)))
-    empty_spaces = len(spaces)
+    empty_spaces = len(empty_spaces_list)
     previous_guesses = []
-    round_counter = 9
+    round_counter = 8
     while empty_spaces != 0:
-        print(" ".join(spaces))
+        if round_counter < 1:
+            print("GAME OVER!")
+            print("My word was {}!".format(guess_word))
+            print("I'VE BEATEN YOU HUMAN!!")
+            print("MUAHAHAHAHA! I WILL TAKE OVER THE WORLD!")
+            break
+        print(" ".join(empty_spaces_list))
         print("You have {} guesses left.".format(round_counter))
         user_character_guess = input("What letter is your guess?  \n").lower()
         if user_character_guess in previous_guesses:
@@ -42,31 +50,25 @@ def man_who_hangs_game(spaces, guess_word):
             occurance_index = find_occurances(guess_word, user_character_guess)
             empty_spaces = empty_spaces - len(occurance_index)
             for occurance in occurance_index:
-                spaces[occurance] = user_character_guess
-                new_empty_spaces_string = " ".join(spaces)
+                empty_spaces_list[occurance] = user_character_guess
+                new_empty_spaces_string = " ".join(empty_spaces_list)
         elif user_character_guess == 'give up':
             print("The word I was using was {}".format(guess_word))
             print("YOU LOSE HUMAN!")
-            break
-        elif round_counter == 1:
-            print("GAME OVER!")
-            print("My word was {}!".format(guess_word))
-            print("I'VE BEATEN YOU HUMAN!!")
-            print("MUAHAHAHAHA! I WILL TAKE OVER THE WORLD!")
             break
         else:
             previous_guesses.append(user_character_guess)
             round_counter = round_counter - 1
             print("That letter isn't in my word! \n")
     if empty_spaces == 0:
-        print(" ".join(spaces))
+        print(" ".join(empty_spaces_list))
         print("YOU BEAT ME HUMAN!!")
 
-guess_word = make_computer_guess_word(available_words)
+#guess_word = make_computer_guess_word(available_words)
 
-empty_spaces_list = get_spaces_list(guess_word)
-
-
+#empty_spaces_list = get_spaces_list(guess_word)
 
 
-man_who_hangs_game(empty_spaces_list, guess_word)
+
+
+man_who_hangs_game(available_words)
