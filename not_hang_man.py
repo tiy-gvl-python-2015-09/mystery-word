2e9f52e808e8a2856f2e20246a0ecc61334623a4
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-18 14:34:19 -0400

    mystery_word.py

--- not_hang_man.py
+++ not_hang_man.py
@@ -1,7 +1,7 @@
 with open('words.txt') as infile:
     list_of_words = infile.read().split()
 
-
+from string import ascii_lowercase
 from random import randint
 
 
@@ -9,13 +9,26 @@ def find_occurences(word, char):
     return [i for i, letter in enumerate(word) if letter == char]
 
 
+def play_again():
+    response = input("Do you want to run this program again? Y/N ").lower()
+    if response == 'y':
+        print("\nLet's go again")
+        not_hang_man()
+        response = ''
+    elif response == 'n':
+        return "Bye."
+        response = ''
+    else:
+        print ("I missed that, what did you say? Y/N")
+        play_again()
+
 def random_word(word_list):
     random_digit = randint(0,len(word_list))
     return word_list[random_digit]
 
 
 def not_hang_man():
-    word = (random_word(list_of_words)).lower()
+    word = random_word(list_of_words).lower()
     letter_list = list(word)
     words_guessed = []
     print ('your word is {} letters long'.format(len(word)))
@@ -28,6 +41,12 @@ def not_hang_man():
         remaining = total_guesses-num_of_guesses
         print('you can guess {} more times'.format(remaining))
         guess = (input('\nguess a letter ').lower())
+        if len(guess) > 1:
+            print ("That's too many")
+            guess = (input('\nguess a letter ').lower())
+        if guess not in ascii_lowercase:
+            print ("That's not a letter")
+            guess = (input('\nguess a letter ').lower())
         if guess not in words_guessed:
             words_guessed.append(guess)
             if guess in letter_list:
@@ -37,12 +56,7 @@ def not_hang_man():
                 print("".join(blank))
                 if ''.join(blank) == word:
                     print('Good Jorb Homegrown!')
-                    response = input("Do you want to run this program again? Y/N ").lower()
-                    if response == 'y':
-                        print("\nLet's go again")
-                        not_hang_man()
-                    if response == 'n':
-                        return "Bye."
+                    break
             else:
                 print("".join(blank))
                 num_of_guesses+=1
@@ -50,12 +64,8 @@ def not_hang_man():
         else:
             print("You already guessed that.")
             print("".join(blank))
-    print('FAILURE! the word was {}'.format(word))
-    response = input("Do you want to run this program again? Y/N ").lower()
-    if response == 'y':
-        print("\nLet's go again")
-        not_hang_man()
-    if response == 'n':
-        return "Bye."
+    if num_of_guesses >= remaining:
+        print('FAILURE! the word was {}'.format(word))
+    play_again()
 
-print (not_hang_man())
+print(not_hang_man())
