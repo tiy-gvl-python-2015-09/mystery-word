41f99e5a80651e1ec8f4f4c292ea9a905ddaa900
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-20 20:05:52 -0400

    mystery_word.py

--- evil_not_hang_man.py
+++ evil_not_hang_man.py
@@ -82,17 +82,17 @@ def difficulty_level():
         return difficulty_level()
 
 
-def azubu_frost():
+def azubu_frost(word_list):
     wordpool = []
-    random_word_length = len(random_word(list_of_words))
+    random_word_length = len(random_word(word_list))
     for word in list_of_words:
         if len(word) == random_word_length:
             wordpool.append(word.lower())
     return wordpool
 
 
-def conami_code(letter):
-    random_words = azubu_frost()
+def conami_code(letter, word_list):
+    random_words = word_list
     new_word_pool = []
     letter_position = {}
     positions = []
@@ -117,43 +117,39 @@ def conami_code(letter):
         for spot in most_occurances:
             if word[spot] is letter:
                 word_pool_redux.append(word)
-    return word_pool_redux
+    return word_pool_redux, most_occurances
 
 
 def not_hang_man():
-    word = azubu_frost(difficulty_level()).lower()
-    letter_list = list(word)
-    words_guessed = []
-    print ('your word is {} letters long'.format(len(word)))
+    word = azubu_frost(difficulty_level())
+    letters_guessed = []
+    print ('your word is {} letters long'.format(len(word[0])))
     num_of_guesses = 0
     total_guesses = 8
     occurance = []
-    blank = list('_' * len(word))
+    blank = list('_' * len(word[0]))
 
     while num_of_guesses < total_guesses:
         remaining = total_guesses-num_of_guesses
         print('you can guess {} more times'.format(remaining))
         guess = (input('\nguess a letter ').lower())
         if len(guess) > 1:
-            print ("That's too many")
+            print ("That's too many letters long.")
             guess = (input('\nguess a letter ').lower())
         if guess not in ascii_lowercase:
-            print ("That's not a letter")
+            print ("That's not a letter.")
             guess = (input('\nguess a letter ').lower())
-        if guess not in words_guessed:
-            words_guessed.append(guess)
-            if guess in letter_list:
-                occurance = find_occurences(letter_list, guess)
-                for position in occurance:
-                    blank[position] = letter_list[position]
-                print("".join(blank))
-                if ''.join(blank) == word:
-                    print('Good Jorb Homegrown!')
-                    break
+        if guess not in letters_guessed:
+            letters_guessed.append(guess)
+            for letter_list in word:
+                if guess in letter_list:
+                    word, occurance = conami_code(guess, word)
+                    for position in occurance:
+                        blank[position] = letter_list[position]
+                    print("".join(blank))
             else:
                 print("".join(blank))
                 num_of_guesses+=1
-
         else:
             print("You already guessed that.")
             print("".join(blank))
@@ -162,4 +158,4 @@ def not_hang_man():
     play_again()
     return "Bye!"
 
-#print(not_hang_man())
+print(not_hang_man())
